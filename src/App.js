8d167087e65e45332da6cd9dc127377ec3e72de9
Pablo Fernández <pablofcasal@gmail.com>
2019-10-22 09:24:23 +0200

    Button

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
-render () {
-
+  
   state = {
 
     name: 'Estado'
   }
 
+render () {
   return (
     <div className="App">
+
+      <button>Change State</button>
+      <br></br>
+      <div>{this.state.name}</div>
       
     </div>
   );
